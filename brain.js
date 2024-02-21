6afa6726cd2dde107b9bf8ef419d243758dae74b
bigDevBosco <nzenwachima10@gmail.com>
2024-02-21 02:32:10 +0100

    Convert to show average possible correct score. Possible outcomes added and also random choice added

--- brain.js
+++ brain.js
@@ -39,15 +39,16 @@ function score() {
 
 
   //based on the logic of concede
-  //getting concede mean and adding extra difficult not get 100%
+
+  //getting concede mean and adding extra difficult not get 100% that is reason of multilplying the mean by 1.5
   let concede_mean = ((gChv + gCav) / 2) * 1.5;
   //using away to determine how home will concede base
-  home_goal_ratio = +gCav / concede_mean;
+  home_goal_ratio = gCav / concede_mean;
   //using home to determine how away will concede
-  away_goal_ratio = +gChv / concede_mean;
+  away_goal_ratio = gChv / concede_mean;
 
   //Multiply personal average goal With opponents average concede
-  home_score_ratio = +gShv * +gCav;
+  home_score_ratio = gShv * gCav;
   away_score_ratio = gSav * gChv;
 
   let totals = (gSav + gShv)
@@ -60,7 +61,7 @@ function score() {
 
 
 function goal_method(a, b) {
-  return +a * +b
+  return a * b
 }
 
 
@@ -68,15 +69,14 @@ function goal_method(a, b) {
 const result = () => {
   score()
 
-  home_goal_1 = goal_method(+home_score_ratio, +home_goal_ratio)
-
+  home_goal_1 = goal_method(home_score_ratio, home_goal_ratio)
   away_goal_1 = goal_method(away_score_ratio, away_goal_ratio)
+
 
   home_goal_2 = goal_method(estimated_goal, estimate_home)
 
   away_goal_2 = goal_method(estimated_goal, estimate_away)
 
-  //Only for Scores
 
   let final_goal_home_1 = goal(home_goal_1)
   let final_goal_away_1 = goal(away_goal_1)
@@ -84,18 +84,83 @@ const result = () => {
   let final_goal_home_2 = goal(home_goal_2)
   let final_goal_away_2 = goal(away_goal_2)
 
+  let goalsTotal1 = final_goal_home_1 + final_goal_away_1
+  let goalsTotal2 = final_goal_home_2 + final_goal_away_2
+
+  let home_total = goal(((final_goal_home_1 + final_goal_home_2) / 2))
+  let away_total = goal(((final_goal_away_1 + final_goal_away_2) / 2))
+  let correct_score = `${home_total}:${away_total}`
+
+
+
+  let suggestions = []
+
+  if (home_total>= 1 && away_total>=1) {
+    suggestions.push("Both Teams to Score")
+  }
+
+  if (home_total > 2 && away_total > 2) {
+    suggestions.push("Over 2.5")
+    suggestions.push("Over 1.5")
+  }
+  if (home_total > 1 && away_total > 1) {
+    if (suggestions.includes("Over 1.5")) {
+
+    } else {
+      suggestions.push("Over 1.5")
+    }
+  }
+
+  if (home_total >= 2 && home_total >= 2) {
+    suggestions.push("Home Score 2")
+    suggestions.push("Home Score 1")
+  }
+
+  if (away_total >= 2 && away_total >= 2) {
+    suggestions.push("Away Score 2")
+    suggestions.push("Away Score 1")
+  }
+
+  if (home_total >= 1 && home_total>= 1) {
+    if (suggestions.includes("Home Score 1")) {} else {
+      suggestions.push("Home Score 1")
+    }
+  }
+
+  if (away_total>= 1 && away_total >= 1) {
+    if (suggestions.includes("Away Score 1")) {} else {
+      suggestions.push("Away Score 1")
+    }
+  }
+
+  if (home_total < 1 && away_total < 1) {
+    suggestions.push("Under 3.5")
+  }
+
+  let random = Math.floor(Math.random() * suggestions.length)
+  let random_Choice = suggestions[random]
+
   display.innerHTML =
     `
-  <p> Scores:</p>
-  <p>${final_goal_home_1}:${final_goal_away_1}</p>
-  
-  <p>${final_goal_home_2}:${final_goal_away_2}
+  <p> possible Correct score:
+  <h3>${correct_score}</h3>
   </p>
+  <p>Possible Outcomes:</p>
   `
+  for (suggestion of suggestions) {
+    display.innerHTML += `<h4>${suggestion}</h4>
+    `
+  }
 
-  console.log(prediction)
+  display.innerHTML += `<p>Random Choice: <h3>${random_Choice}</h3></p>
+`
+
 }
 
+
+/*
+Function below is used to approximate the possible goal
+*/
 
 function goal(goal) {
   let result;
